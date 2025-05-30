04b4a52ab3c4ac74e4d280752229b167b793cb09
TodorP <tose.pljakov@gmail.com>
2025-05-30 14:23:22 +0200

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,10 @@
+//Modules
+
+const names = require('./4-names.js');
+const sayHi = require('./5-utils.js');
+const data = require('./6-alternative-flavour.js');
+require('./7-mind-granade.js');
+
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
